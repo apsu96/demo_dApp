17842a901f468e34b88ed9d10cc3c6718ddaceda
apsu96 <apsu0161@gmail.com>
2024-12-23 08:35:08 -0600

    feat: add connectors

--- src/app/core/wagmiConfig.ts
+++ src/app/core/wagmiConfig.ts
@@ -1,8 +1,14 @@
 import { http, createConfig } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
+import { injected, coinbaseWallet, metaMask } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
   chains: [baseSepolia],
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: "Demo DApp" }),
+    metaMask({ dappMetadata: { name: "Demo DApp" } }),
+  ],
   transports: {
     [baseSepolia.id]: http(),
   },
