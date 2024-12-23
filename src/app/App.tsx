4e93917773e7d3aa96ad6b9e0a6c90f3be1e2a09
apsu96 <apsu0161@gmail.com>
2024-12-23 11:47:35 -0600

    feat: add header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { useAccount } from "wagmi";
 import Account from "./components/Account";
 import ConnectWallet from "./components/ConnectWallet";
+import Header from "./components/Header";
 
 const App = () => {
   const { isConnected } = useAccount();
   return (
-    <div className="max-w-screen-xl">
+    <div className="min-h-screen container mx-auto px-4 flex flex-col">
+      <Header />
       {isConnected ? <Account /> : <ConnectWallet />}
     </div>
   );
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mt-14 p-4">
+      <h1 className="text-3xl font-semibold">DEMO DAPP</h1>
+    </div>
+  );
+};
+
+export default Header;
