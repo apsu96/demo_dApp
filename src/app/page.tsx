9b98695cfe67bfa279f1590040ff52f022be1dfb
apsu96 <apsu0161@gmail.com>
2024-12-23 08:12:44 -0600

    feat: add wagmi provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "./core/wagmiConfig";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <div></div>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
