0ee3d6b8b4311d00b53d14d8db924c0e1ab76b0e
apsu96 <apsu0161@gmail.com>
2024-12-24 14:52:02 -0600

    feat: close modal

--- src/app/components/ConnectModal.tsx
+++ src/app/components/ConnectModal.tsx
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { EventHandler, useEffect, useRef } from "react";
 import { useConnect } from "wagmi";
 
-const ConnectModal = ({ isOpen }: { isOpen: boolean }) => {
-  const { connectors, connect } = useConnect();
+const ConnectModal = ({
+  isOpen,
+  handleClose,
+}: {
+  isOpen: boolean;
+  handleClose: () => void;
+}) => {
+  const { connect } = useConnect();
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    function handleClickOutside(e: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        handleClose();
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
   return (
     <div
       className={
         "fixed inset-0 z-50 flex flex-col justify-center items-center " +
-        (isOpen ? "flex backdrop-blur" : "hidden")
+        (isOpen
+          ? "flex backdrop-blur transition ease-in-out duration-150"
+          : "hidden")
       }
     >
       <div
+        ref={modalRef}
         className={
-          "flex-col bg-gray-100 px-12 py-16 rounded-3xl " +
+          "relative flex-col bg-gray-100 px-12 py-14 rounded-3xl " +
           (isOpen ? "flex" : "hidden")
         }
       >
+        <div
+          className="absolute top-5 right-8 cursor-pointer ml-auto hover:bg-white"
+          onClick={handleClose}
+        >
+          <img
+            src="/images/closeIcon.svg"
+            alt="Close Icon"
+            width={24}
+            height={24}
+          />
+        </div>
         <h1 className="text-3xl font-semibold text-center">DEMO DAPP</h1>
         <div className="flex flex-col mt-3 w-full gap-4">
           <p className="text-xl mb-4">Connect your wallet</p>
--- src/app/components/ConnectWallet.tsx
+++ src/app/components/ConnectWallet.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useConnect } from "wagmi";
 import ConnectModal from "./ConnectModal";
 
 const ConnectWallet = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClose = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
 
   return (
     <div className="flex flex-col items-center gap-6 m-4 p-14 bg-orange-300 rounded-2xl">
@@ -14,7 +18,7 @@ const ConnectWallet = () => {
       >
         Connect
       </button>
-      <ConnectModal isOpen={isModalOpen} />
+      <ConnectModal isOpen={isModalOpen} handleClose={handleClose} />
     </div>
   );
 };
